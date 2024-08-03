1f44c2e0351af7cf85bee071047bd81726a7aca1
Paula García <pdgarciamiranda@gmail.com>
2024-08-03 18:58:31 -0500

    desafio stickers resuelto

--- assets2/stickers.js
+++ assets2/stickers.js
@@ -1,11 +1,18 @@
-let input1 = Number(document.querySelector('.i1')).value
-let input2 = Number(document.querySelector('.i2')).value
-let input3 = Number(document.querySelector('.i3')).value
-
 const suma = () => {
-    let resultado = input.value
-    if (input1 + input2 + input3 > 10) {
-    resultado = document.querySelector('p');
-    resultado.innerHTML = "Llevas demasiados stickers";
-}
-}
+
+let input1 = Number(document.querySelector(".i1").value);
+let input2 = Number(document.querySelector(".i2").value);
+let input3 = Number(document.querySelector(".i3").value);
+
+let resultado = input1 + input2 + input3;
+/* console.log(resultado); */
+let respuesta = document.querySelector("p");
+
+  if (resultado > 10) {
+    respuesta.innerHTML = "Llevas demasiados stickers";
+  } else if (resultado <= 10) {
+    respuesta.innerHTML = `Llevas ${resultado} stickers`;
+  } else {
+    respuesta.innerHTML = "Debes números que sumen 10 stickers en total";
+  }
+};
